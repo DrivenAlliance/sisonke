64ebb4164f0d13b0ad7b30da65e49cfef534b29c
Theo Bohnen <tbohnen.jnr@gmail.com>
2016-06-25 10:38:10 +0200

    Fix error with missing view

--- DjangoTutorial/SisonkeDjangoTutorial/polls/urls.py
+++ DjangoTutorial/SisonkeDjangoTutorial/polls/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
-    url(r'^something', views.something, name='something'),
 ]
